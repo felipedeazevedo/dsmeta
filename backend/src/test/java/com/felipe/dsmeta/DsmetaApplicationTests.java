fe536317f6c250472c033dbca61209c18278f2b7
fazevedo959@gmail.com <fazevedo959@gmail.com>
2022-09-05 15:50:57 -0300

    Project created.

---
+++ backend/src/test/java/com/felipe/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
