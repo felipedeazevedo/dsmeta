afd4bb86d03250448049e449a113eb122e14c2b9
fazevedo959@gmail.com <fazevedo959@gmail.com>
2022-09-06 09:29:51 -0300

    useState

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,5 +1,5 @@
-import logo from '../../assets/img/logo.svg'
-import './styles.css'
+import logo from '../../assets/img/logo.svg';
+import './styles.css';
 
 function Header() {
     return (
@@ -16,7 +16,7 @@ function Header() {
     )
 }
 
-export default Header
+export default Header;
 
 
 
